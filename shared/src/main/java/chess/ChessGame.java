f176694db3984818510b4d1169195036e921d854
Porter Schollenberger <porterschollenberger@gmail.com>
2024-05-11 15:15:06 -0600

    Fixed isInStalemate to check for valid moves instead of all moves

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -184,7 +184,11 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        return !isInCheck(teamColor) && (getAllMovesByTeam(teamColor).isEmpty());
+        Collection<ChessMove> validatedMoves = new ArrayList<>();
+        for (ChessMove move : getAllMovesByTeam(teamColor)) {
+            validatedMoves.addAll(validMoves(move.getStartPosition()));
+        }
+        return !isInCheck(teamColor) && (validatedMoves.isEmpty());
     }
 
     /**
