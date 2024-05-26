d9081533dcb0a51985f9f0e86a96c81f3fdb33a6
Porter Schollenberger <porterschollenberger@gmail.com>
2024-05-26 13:53:02 -0600

    Set up Server to initialize service classes and register endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,18 +1,42 @@
 package server;
 
+import dataaccess.*;
+import handler.HandlerRegistry;
+import memory.*;
+import service.*;
 import spark.*;
 
 public class Server {
 
     public int run(int desiredPort) {
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+        UserDAO userDAO = new MemoryUserDAO();
+
+        UserService userService = new UserService(authDAO, userDAO);
+        ClearService clearService = new ClearService(authDAO, gameDAO, userDAO);
+        GameService gameService = new GameService(authDAO, gameDAO);
+
         Spark.port(desiredPort);
 
-        Spark.staticFiles.location("web");
+        Spark.staticFiles.location("/web");
 
-        // Register your endpoints and handle exceptions here.
+        HandlerRegistry handlerRegistry = new HandlerRegistry(clearService, gameService, userService);
+        registerEndpoints(handlerRegistry);
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private void registerEndpoints(HandlerRegistry handlerRegistry) {
+        Spark.get("/hello", (req, res) -> "Hello BYU!");
+        Spark.delete("/db", (req, res) -> handlerRegistry.getHandler("clear").handle(req, res));
+        Spark.post("/user", (req, res) -> handlerRegistry.getHandler("register").handle(req, res));
+        Spark.post("/session", (req, res) -> handlerRegistry.getHandler("login").handle(req, res));
+        Spark.delete("/session", (req, res) -> handlerRegistry.getHandler("logout").handle(req, res));
+        Spark.get("/game", (req, res) -> handlerRegistry.getHandler("listGames").handle(req, res));
+        Spark.post("/game", (req, res) -> handlerRegistry.getHandler("createGame").handle(req, res));
+        Spark.put("/game", (req, res) -> handlerRegistry.getHandler("joinGame").handle(req, res));
     }
 
     public void stop() {
