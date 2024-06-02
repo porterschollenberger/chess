cc718f7d7bafe49c3668c7a77bb9ec8b0e5f66c9
Porter Schollenberger <porterschollenberger@gmail.com>
2024-06-01 18:46:10 -0600

    Implemented SQLAuthDAO and made methods of DatabaseManager public for use outside package

--- server/src/main/java/sql/SQLAuthDAO.java
+++ server/src/main/java/sql/SQLAuthDAO.java
@@ -1,31 +1,134 @@
 package sql;
 
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
 import model.AuthData;
 
+import java.sql.SQLException;
+import java.util.UUID;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+
 public class SQLAuthDAO implements AuthDAO {
+    public SQLAuthDAO() {
+        try {
+            configureDatabase();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public String createAuth(String username) {
-        return null;
+        var statement = "INSERT INTO authData (authToken, username) VALUES (?, ?)";
+        String authToken = UUID.randomUUID().toString();
+        try {
+            executeUpdate(statement, authToken, username);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return authToken;
     }
 
     @Override
     public AuthData getAuth(String authToken) {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT authToken, username FROM authData WHERE authToken=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return new AuthData(rs.getString(1), rs.getString(2));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
     @Override
     public void deleteAuth(String authToken) {
-
+        var statement = "DELETE FROM authData WHERE authToken=?";
+        try {
+            executeUpdate(statement, authToken);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void clear() {
-
+        var statement = "TRUNCATE TABLE authData";
+        try {
+            executeUpdate(statement);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        var statement = "SELECT * FROM authData";
+        try {
+            var rs = executeUpdate(statement);
+            assert rs != null;
+            return rs.isEmpty();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getString(1);
+                }
+
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS authData (
+              `authToken` varchar(256) NOT NULL,
+              `username` varchar(256) NOT NULL,
+              PRIMARY KEY (`authToken`),
+              INDEX(authToken),
+              INDEX(username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to configure database: " + e.getMessage());
+        }
     }
 }
