d6f53468eb0224eb02799000a6eae0721937d365
Porter Schollenberger <porterschollenberger@gmail.com>
2024-06-03 17:54:34 -0600

    Added tests for GameDAO, needs work

---
+++ server/src/test/java/dataaccess/GameDAOTest.java
@@ -0,0 +1,90 @@
+package dataaccess;
+
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sql.SQLGameDAO;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameDAOTest {
+    private GameDAO gameDAO;
+
+    @BeforeEach
+    void setUp() {
+        gameDAO = new SQLGameDAO();
+        gameDAO.clear();
+    }
+
+    @Test
+    void testCreateGameSuccess() {
+        String gameName = "test game name";
+
+        Integer gameID = gameDAO.createGame(gameName);
+
+        assertNotNull(gameID);
+    }
+
+    @Test
+    void testCreateGameFailure() {
+        assertThrows(RuntimeException.class, () -> gameDAO.createGame(null));
+    }
+
+
+
+    @Test
+    void testGetGameSuccess() {
+        String gameName = "test game name";
+        int gameID = gameDAO.createGame(gameName);
+
+        GameData game = gameDAO.getGame(gameID);
+
+        assertNotNull(game);
+    }
+
+    @Test
+    void testGetGameFailure() {
+        //TODO: find a way that this fails so I can test for that
+    }
+
+    @Test
+    void testListGamesSuccess() {
+        Collection<GameData> games = gameDAO.listGames();
+
+        assertNotNull(games);
+    }
+
+    @Test
+    void testListGamesFailure() {
+        //TODO: find a way that this fails so I can test for that
+    }
+
+    @Test
+    void testUpdateGameSuccess() {
+        int gameID = gameDAO.createGame("test game name");
+        String username = "test username";
+
+        gameDAO.updateGame(gameID, "whiteUsername", username);
+
+        assertEquals(gameDAO.getGame(gameID).whiteUsername(), username);
+    }
+
+    @Test
+    void testUpdateGameFailure() {
+        int gameID = gameDAO.createGame("test game name");
+        String username = "test username";
+
+        assertThrows(IllegalArgumentException.class, () -> gameDAO.updateGame(gameID, "ErrorInducingMistake", username));
+    }
+
+    @Test
+    void testClearSuccess() {
+        gameDAO.createGame("test game");
+
+        gameDAO.clear();
+
+        assertTrue(gameDAO.isEmpty());
+    }
+}
