34ccaf91e75be88a1c9946dac1ed7370bcd4d674
Porter Schollenberger <porterschollenberger@gmail.com>
2024-06-10 13:28:12 -0600

    Server and Client now run independently

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,13 +1,7 @@
-import chess.*;
 import client.Repl;
-import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server();
-        var port = server.run(0);
-        System.out.println("Started test HTTP server on " + port);
-
-        new Repl(port).run();
+        new Repl(8080).run();
     }
 }
