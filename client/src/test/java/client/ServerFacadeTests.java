327acf22f5142ca699cedea4309aa07967d00746
Porter Schollenberger <porterschollenberger@gmail.com>
2024-06-10 01:28:23 -0600

    Added register, login, and logout tests for ServerFacadeTests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,25 @@
 package client;
 
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
+        facade.clear();
     }
 
     @AfterAll
@@ -22,8 +29,26 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void testRegister() throws Exception {
+        var registerResponse = facade.register(new UserData("player1", "password", "p1@email.com"));
+        assertNotNull(registerResponse.getAuthToken());
+    }
+
+    @Test
+    void testLogin() throws Exception {
+        facade.register(new UserData("player1", "password", "p1@email.com"));
+
+        var loginResponse = facade.login("player1", "password");
+        assertNotNull(loginResponse.getAuthToken());
+    }
+
+    @Test
+    void testLogout() throws Exception {
+        facade.register(new UserData("player1", "password", "p1@email.com"));
+        facade.login("player1", "password");
+
+        var GenericResponse = facade.logout();
+        assertNotNull(GenericResponse.getMessage());
     }
 
 }
